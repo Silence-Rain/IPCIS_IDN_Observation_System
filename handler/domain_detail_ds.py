f90655fa6d3d7fe4a22380bd26f5a0e4529e56b9
Silence <daniel.s.mo503@gmail.com>
2018-03-29 15:42:44 +0800

    更新数据表结构，添加对应列的handler

--- handler/domain_detail_ds.py
+++ handler/domain_detail_ds.py
@@ -13,19 +13,18 @@ def read_data():
 		for line in f.readlines():
 			temp = line.strip().split("=")
 			temp[0] = temp[0].strip()
-			# temp[1] = temp[1].split(",")
 			pds.append(temp[0])
 
 def create_db():
-	sql = ("CREATE TABLE IF NOT EXISTS domain_detail ( domain_id INT(10) UNSIGNED NOT NULL DEFAULT 0," 
+	sql = ("CREATE TABLE IF NOT EXISTS bad_domain_detail ( domain_id INT(10) UNSIGNED NOT NULL DEFAULT 0," 
 		"primary_domain VARCHAR(128) NOT NULL DEFAULT '', "
 		"is_dga INT(10) UNSIGNED NOT NULL DEFAULT 0, "
-     	"ttl INT(10) UNSIGNED NOT NULL DEFAULT 0, "
-	    "registrar VARCHAR(64) DEFAULT '', "
-	    "registrant VARCHAR(64) DEFAULT '', "
+     	"ttl INT(10) UNSIGNED NOT NULL, "
+     	"credit INT(10) UNSIGNED, "
+	    "register_location VARCHAR(32) DEFAULT '', "
 	    "register_length VARCHAR(16) DEFAULT '', "
-	    "ip_active INT(10) UNSIGNED DEFAULT 0, "
-	    "ip_port TEXT, "
+	    "ip_active INT(10) UNSIGNED, "
+	    "ip_port VARCHAR(255) DEFAULT '', "
 	    "PRIMARY KEY(domain_id, primary_domain) "
 	    " ) ENGINE=INNODB DEFAULT CHARSET=utf8;")
 
@@ -37,9 +36,9 @@ def create_db():
 		raise e
 
 def init_db():
-	# 初始化对应的域名基本信息
+	# 初始化对应的域名基本信息（原始数据）
 	for item in pds:
-		sql_name = ("INSERT INTO domain_detail (domain_id, primary_domain, is_dga, ttl) "
+		sql_name = ("INSERT INTO bad_domain_detail (domain_id, primary_domain, is_dga, ttl) "
 			"SELECT domain_id, primary_domain, is_dga, ttl FROM domain_name "
 			"WHERE primary_domain = '%s';" % item)
 		
@@ -51,15 +50,15 @@ def init_db():
 			raise e
 
 	# 初始化对应的whois信息
-	sql_whois = ("UPDATE domain_detail as a INNER JOIN domain_whois as b "
-		"on a.primary_domain=b.primary_domain SET "
-		"a.registrar=b.registrar, a.registrant=b.registrant;")
-	try:
-		mysql.cursor.execute(sql_whois)
-		mysql.conn.commit()
-	except Exception as e:
-		mysql.conn.rollback()
-		raise e
+	# sql_whois = ("UPDATE domain_detail as a INNER JOIN domain_whois as b "
+	# 	"on a.primary_domain=b.primary_domain SET "
+	# 	"a.registrar=b.registrar, a.registrant=b.registrant;")
+	# try:
+	# 	mysql.cursor.execute(sql_whois)
+	# 	mysql.conn.commit()
+	# except Exception as e:
+	# 	mysql.conn.rollback()
+	# 	raise e
 
 def add_register_length():
 	times = []
@@ -78,8 +77,6 @@ def add_register_length():
 			times.append([])
 
 	for item in times:
-		# if len(item) != 0:
-		# 	if item[0] != '' and item[1] != '':
 		try:	
 			temp = utils.standard2timestamp(item[1]) - utils.standard2timestamp(item[0])
 			time_diff.append(utils.timestamp2diff(temp))
@@ -87,7 +84,7 @@ def add_register_length():
 			time_diff.append('')
 
 	for i in range(0, len(pds)):
-		sql_set = ("UPDATE domain_detail SET register_length = '%s' "
+		sql_set = ("UPDATE bad_domain_detail SET register_length = '%s' "
 		"WHERE primary_domain = '%s';") % (time_diff[i], pds[i])
 
 		try:
@@ -97,14 +94,46 @@ def add_register_length():
 			mysql.conn.rollback()
 			raise e
 
+def add_credit_evidence():
+	ret = []
+	scoreDict = {"safe": 100, "unsure": 70}
+
+	for pd in pds:
+		sql = "SELECT evidence FROM domain_name WHERE primary_domain='%s';" % pd
+
+		mysql.cursor.execute(sql)
+		rs = mysql.cursor.fetchone()
+
+		if rs != None:
+			arr = rs[0].split("\"")[2:]
+			temp = None
+			for item in arr:
+				if item == "safe" or item == "unsure":
+					temp = scoreDict[item]
+
+			ret.append(temp)
+		else:
+			ret.append(None)
+
+	for item in ret:
+		sql_update = "UPDATE bad_domain_detail SET credit=%d;" % item
+
+		try:
+			mysql.cursor.execute(sql_update)
+			mysql.conn.commit()
+		except Exception as e:
+			mysql.conn.rollback()
+			raise e
+			
 
 if __name__ == "__main__":
 	try:
 		read_data()
-		#create_db()
-		#init_db()
+		create_db()
+		init_db()
 		add_register_length()
-	# except Exception as e:
-	# 	print(e, time.asctime(time.localtime(time.time())))
+		add_credit_evidence()
+	except Exception as e:
+		print(e, time.asctime(time.localtime(time.time())))
 	finally:
 		mysql.conn.close()
