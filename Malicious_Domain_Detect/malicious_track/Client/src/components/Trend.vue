d199e2b22396eec524c1512e8690bca62c874421
Silence <daniel.s.mo503@gmail.com>
2018-04-23 00:47:08 +0800

    完成流量趋势折线图

--- Malicious_Domain_Detect/malicious_track/Client/src/components/Trend.vue
+++ Malicious_Domain_Detect/malicious_track/Client/src/components/Trend.vue
@@ -1,5 +1,12 @@
 <template>
 	<div>
+		<div class="label">
+			<div>近7天IP流量趋势</div>
+			<Button type="primary" class="btn" @click="download">
+				<Icon type="ios-cloud-download" size="15" color="#fff"></Icon>
+				下载
+			</Button>
+		</div>
 		<keep-alive>
 			<div id="chart" style="width:600px;height:400px;"></div>
 		</keep-alive>
@@ -17,16 +24,12 @@
 		mounted () {
 			let echarts = require("echarts/lib/echarts")
 			require('echarts/lib/chart/line')
-			require('echarts/lib/component/tooltip')
-			require('echarts/lib/component/legend')
 
 			let chart = echarts.init(document.getElementById("chart"))
 			chart.setOption({
-				title: {
-					text: "近7天IP流量趋势"
-				},
 				xAxis: {
 			        type: 'category',
+			        boundaryGap: false,
 			        data: ['4/17', '4/18', '4/19', '4/20', '4/21', '4/22', '4/23']
 			    },
 			    yAxis: {
@@ -34,13 +37,32 @@
 			    },
 			    series: [{
 			        data: [50, 65, 60, 80, 45, 70, 100],
-			        type: 'line'
+			        type: 'line',
+			        areaStyle: {color: "#F4A460"}
 			    }]
 			})
+		},
+
+		methods: {
+			download () {
+				
+			}
 		}
 	}
 </script>
 
 <style scoped>
-
+.label{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+    margin: 10px;
+    font-size: 16px;
+}
+.btn{
+	width: 70px;
+	height: 30px;
+	margin-left: 50px;
+	font-size: 12px;
+}
 </style>
