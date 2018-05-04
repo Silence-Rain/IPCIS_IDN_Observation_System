ef0426e871e86d43c4d8937dcffefc2a90ff9b5a
Silence <daniel.s.mo503@gmail.com>
2018-05-05 00:31:49 +0800

    完成ip活动地理位置图的绘制

--- Malicious_Domain_Detect/malicious_track/Client/src/components/Map.vue
+++ Malicious_Domain_Detect/malicious_track/Client/src/components/Map.vue
@@ -1,15 +1,156 @@
 <template>
 	<div>
-		Map
+		<div class="label">
+			<div>IP活动地理位置分布</div>
+			<Button type="primary" class="btn" @click="download">
+				<Icon type="ios-cloud-download" size="15" color="#fff"></Icon>
+				下载
+			</Button>
+		</div>
+		<keep-alive>
+			<div id="chart" style="width:600px;height:400px;"></div>
+		</keep-alive>
 	</div>
 </template>
 
 <script>
-	export default {
+	import inMap from "inmap/dist/inmap.min.js"
+	import {MP} from '../js/map.js'
+	import axios from 'axios'
 
+	export default {
+		data () {
+			return {
+				inMap: null,
+				acts: [
+				{
+					from: {
+						city: "118.89.140.118",
+						lnglat: [112.95, 28.43]
+					},
+					to: {
+						city: "211.65.193.23",
+						lnglat: [121.48, 31.22]
+					}
+				},
+				{
+					from: {
+						city: "118.89.140.118",
+						lnglat: [112.95, 28.43]
+					},
+					to: {
+						city: "192.168.1.1",
+						lnglat: [113.27, 21.13]
+					}
+				},
+				{
+					from: {
+						city: "202.112.23.167",
+						lnglat: [116.41, 39.91]
+					},
+					to: {
+						city: "118.89.140.118",
+						lnglat: [112.95, 28.43]
+					}
+				}
+				]
+
+			}
+		},
+
+		created () {
+			// let url = "http://211.65.197.210:8080/IPCIS/activityDatabase/?" + 
+			// 		"IpSets=211.65.193.193:32-211.65.197.210:32-202.112.23.167:32-211.65.192.172:32" + 
+			// 		"&TableName=2018-05-03&Mode=1"
+
+			// axios.get(url)
+			// 	.then((response) => {
+			// 		console.log(response);
+			// 	})
+			// 	.catch((response) => {
+			// 		console.log("000");
+			// 	});
+
+			// this.$nextTick(() => {
+			// 	var that = this
+
+			// 	MP().then(BMap => {
+			// 		this.inMap = new inMap.Map({
+			// 			id: "chart",
+			// 			skin: "Blueness",
+			// 			center: [107.40, 33.42],
+			// 			zoom: {
+			// 				value: 4, 
+			// 				show: true, 
+			// 				max: 13, 
+			// 				min: 4
+			// 			},
+			// 		})
+
+			// 		let overlay = new inMap.MoveLineOverlay({
+			// 			data: this.acts
+			// 		});
+
+			// 		console.log(this.inMap)
+
+			// 		this.inMap.add(overlay)
+			// 		console.log(this.inMap)
+			// 	})
+			// })
+		},
+
+		mounted () {
+			// let echarts = require("echarts/lib/echarts")
+			// require('echarts/lib/chart/map')
+
+			// let chart = echarts.init(document.getElementById("chart"))
+			var that = this
+			this.$nextTick(() => {
+
+				MP().then(BMap => {
+					that.inMap = new inMap.Map({
+						id: "chart",
+						skin: "Blueness",
+						center: [107.40, 33.42],
+						zoom: {
+							value: 2, 
+							show: true, 
+							max: 10, 
+							min: 2
+						},
+					})
+
+					let overlay = new inMap.MoveLineOverlay({
+						data: that.acts
+					});
+
+					that.inMap.add(overlay)
+					console.log(that.inMap === this.inMap)
+				})
+			})
+		},
+
+		methods: {
+			download () {
+
+			}
+		}
 	}
+
 </script>
 
 <style scoped>
-
+.label{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+    margin: 10px;
+    font-size: 16px;
+}
+.btn{
+	width: 70px;
+	height: 30px;
+	margin-left: 50px;
+	font-size: 12px;
+}
 </style>
--- Malicious_Domain_Detect/malicious_track/Client/src/components/Relation.vue
+++ Malicious_Domain_Detect/malicious_track/Client/src/components/Relation.vue
@@ -1,15 +1,111 @@
 <template>
 	<div>
-		Relation
+		<div class="label">
+			<div>通信对象关系</div>
+			<Button type="primary" class="btn" @click="download">
+				<Icon type="ios-cloud-download" size="15" color="#fff"></Icon>
+				下载
+			</Button>
+		</div>
+		<keep-alive>
+			<div id="chart" style="width:400px;height:400px;"></div>
+		</keep-alive>
 	</div>
 </template>
 
 <script>
 	export default {
+		data () {
+			return {
+				data: [
+                {
+                    name: '118.89.140.118',
+                    value: 6,
+                    children: [
+                        {
+                            name: '211.65.193.23',
+                            value: 4
+                        },
+                        {
+                            name: '192.168.1.1',
+                            value: 4
+                        },
+                        {
+                            name: '202.112.23.167',
+                            value: 4
+                        }
+                    ]
+                }
+            ]
+			}
+		},
 
+		mounted () {
+			let echarts = require("echarts/lib/echarts")
+			require('echarts/lib/chart/tree')
+
+			let chart = echarts.init(document.getElementById("chart"))
+			chart.setOption({
+				series: [
+		         	{
+		                type: 'tree',
+
+		                data: this.data,
+
+		                top: '1%',
+		                left: '7%',
+		                bottom: '1%',
+		                right: '20%',
+
+		                symbolSize: 7,
+
+		                label: {
+		                    normal: {
+		                        position: 'left',
+		                        verticalAlign: 'middle',
+		                        align: 'right',
+		                        fontSize: 9
+		                    }
+		                },
+
+		                leaves: {
+		                    label: {
+		                        normal: {
+		                            position: 'right',
+		                            verticalAlign: 'middle',
+		                            align: 'left'
+		                        }
+		                    }
+		                },
+
+		                expandAndCollapse: true,
+		                animationDuration: 550,
+		                animationDurationUpdate: 750
+		            }
+		        ]
+			})
+		},
+
+		methods: {
+			download () {
+				
+			}
+		}
 	}
 </script>
 
 <style scoped>
-
+.label{
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+    margin: 10px;
+    font-size: 16px;
+}
+.btn{
+	width: 70px;
+	height: 30px;
+	margin-left: 50px;
+	font-size: 12px;
+}
 </style>
