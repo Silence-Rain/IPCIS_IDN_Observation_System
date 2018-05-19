214aad80eb50886f180f9d92664679edcada4204
Silence <daniel.s.mo503@gmail.com>
2018-05-19 18:10:16 +0800

    topo接口返回节点中添加count字段

--- malicious_detection/detection_system/server/utils/graph.py
+++ malicious_detection/detection_system/server/utils/graph.py
@@ -1,6 +1,7 @@
 def raw2date_dict(data):
 	keys = []
 	values = []
+
 	for item in data:
 		if item[-1] in keys:
 			values[keys.index(item[-1])].append(tuple(item[:-2]))
@@ -13,10 +14,31 @@ def raw2date_dict(data):
 def raw2count(data):
 	ret = {}
 
-def node_constructor(nodes):
+	for item in data:
+		if item[-1] not in ret:
+			ret[item[-1]] = [{item[0]:0}]
+		ret[item[-1]].append({item[1]:item[2]})
+		ret[item[-1]][0][item[0]] += item[2]
+
+	return flatten_dict(ret)
+
+def flatten_dict(data):
+	ret = {}
+
+	for date in data.values():
+		for item in date:
+			ip = list(item.keys())[0]
+			if ip not in ret:
+				ret[ip] = item[ip]
+			else:
+				ret[ip] += item[ip]
+
+	return ret
+
+def node_constructor(nodes, counts):
 	ret = []
 	for index, item in enumerate(nodes):
-		ret.append({"id": index, "name": item})
+		ret.append({"id": index, "name": item, "count": counts[item]})
 
 	return ret
 
@@ -29,7 +51,9 @@ def link_constructor(nodes, links):
 
 def steady_topo(data):
 	dic = raw2date_dict(data)
+	count_map = raw2count(data)
 	intersect = set()
+
 	for index, date in enumerate(dic.keys()):
 		for item in dic[date]:
 			if index == 0:
@@ -43,14 +67,16 @@ def steady_topo(data):
 	for item in intersect:
 		node_set.update(item)
 	nodes_raw = list(node_set)
-	nodes = node_constructor(nodes_raw)
+	nodes = node_constructor(nodes_raw, count_map)
 	links = link_constructor(nodes_raw, list(intersect))
 
 	return {"nodes": nodes, "links": links}
 
 def max_topo(data):
 	dic = raw2date_dict(data)
+	count_map = raw2count(data)
 	intersect = set()
+
 	for index, date in enumerate(dic.keys()):
 		for item in dic[date]:
 			if index == 0:
@@ -64,7 +90,7 @@ def max_topo(data):
 	for item in intersect:
 		node_set.update(item)
 	nodes_raw = list(node_set)
-	nodes = node_constructor(nodes_raw)
+	nodes = node_constructor(nodes_raw, count_map)
 	links = link_constructor(nodes_raw, list(intersect))
 
 	return {"nodes": nodes, "links": links}
