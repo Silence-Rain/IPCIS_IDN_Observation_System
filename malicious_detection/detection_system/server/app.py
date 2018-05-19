19d2c55a4bcd2275e640cefbd077cb75ac0213c6
Silence <daniel.s.mo503@gmail.com>
2018-05-19 22:20:51 +0800

    限制跨域

--- malicious_detection/detection_system/server/app.py
+++ malicious_detection/detection_system/server/app.py
@@ -9,7 +9,7 @@ from MySQL.mysql import MySQL
 
 define("port", default=8888, help="本地监听端口", type=int)
 define("DEBUG", default=True, help="是否开启debug模式", type=bool)
-define("TEST", default=True, help="测试服务器,支持跨域访问,推送测试模式", type=bool)
+define("TEST", default=False, help="测试服务器,支持跨域访问,推送测试模式", type=bool)
 tornado.options.parse_command_line()
 
 dns = MySQL(
