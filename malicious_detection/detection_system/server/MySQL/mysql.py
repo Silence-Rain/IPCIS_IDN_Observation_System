79e4bab56fa46fd20f5cf47a07869c3287d240ef
Silence <daniel.s.mo503@gmail.com>
2018-05-23 19:31:19 +0800

    数据库自动重连ing

--- malicious_detection/detection_system/server/MySQL/mysql.py
+++ malicious_detection/detection_system/server/MySQL/mysql.py
@@ -1,18 +1,18 @@
 #!coding=utf8
 
 import pymysql
+import time
+import sched
 
 class MySQL(object):
 	def __init__(self, host, user, passwd, db, port=3306, charset='utf8'):
-		self.conn = pymysql.connect(
-						host = host, 
-						port = port, 
-						user = user,
-						passwd = passwd,
-						db = db,
-						charset = charset
-						)
-		self.cursor = self.conn.cursor()
+		self.host = host
+		self.user = user
+		self.passwd = passwd
+		self.db = db
+		self.port = port
+		self.charset = charset
+		self.connect()
 
 	async def get(self, sql):
 		self.cursor.execute(sql)
@@ -28,6 +28,23 @@ class MySQL(object):
 			self.conn.commit()
 		except Exception as e:
 			self.conn.rollback()
+
+	def connect(self):
+		self.conn = pymysql.connect(
+						host = self.host, 
+						port = self.port, 
+						user = self.user,
+						passwd = self.passwd,
+						db = self.db,
+						charset = self.charset
+						)
+		self.cursor = self.conn.cursor()
+
+	def refresh(self):
+		self.close()
+		s = sched.scheduler(time.time, time.sleep)
+		s.enter(28000, self.connect)
+		s.run()
 
 	def close(self):
 		self.cursor.close()
